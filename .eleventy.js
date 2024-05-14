f2862db90225aeed2b1f679df32757c36c989de0
Leonardo Ramírez Gutiérrez <150188420+LeeonDeChino@users.noreply.github.com>
2024-05-14 17:38:38 -0600

    Otro commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports  = function (eleventyConfig){
+eleventyConfig.addPassthroughCopy("code/css");
+eleventyConfig.addPassthroughCopy("code/js");
+eleventyConfig.addPassthroughCopy("code/img");
+
   return{
     dir:{
       input: "code",
